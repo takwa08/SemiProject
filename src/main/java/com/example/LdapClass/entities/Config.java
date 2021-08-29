7c01709fdb57dfba51c4225310530b0bbab3585a
Takwa Assaibi <takwaassaibi08@gmail.com>
2021-08-29 18:13:39 +0100

    changed file
    j'ai changé le nom de base exemple c'est pas une grande chose, et j'ai ajouté le user dn et le password dans fichier config

--- src/main/java/com/example/LdapClass/entities/Config.java
+++ src/main/java/com/example/LdapClass/entities/Config.java
@@ -1,28 +1,44 @@
 package com.example.LdapClass.entities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
 import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
 
+
 @Configuration
 @EnableLdapRepositories
+@PropertySource("classpath:application.properties")
 public class Config {
 
-	@Bean
-	public LdapContextSource ldapContextSource() {
-		LdapContextSource LCS = new LdapContextSource();
-		LCS.setUrl("ldap://localhost:10389");
-		LCS.setBase("dc=MAKTAKM,dc=tn");
-		return LCS;
-	}
-	
-	@Bean
-	public LdapTemplate ldapTemplate() {
-		return new LdapTemplate(ldapContextSource());
-	}
+	 @Autowired
+	  
 
+	  
+
+	    @Bean
+	    public LdapContextSource contextSource() {
+	        LdapContextSource contextSource = new LdapContextSource();
+	        contextSource.setUrl("ldap://localhost:10381");
+	        contextSource.setBase("dc=exemple,dc=com");
+	        contextSource.setUserDn("uid=admin,ou=system");
+	        contextSource.setPassword("secret");
+	        return contextSource;
+	    }
+
+	    @Bean
+	    public LdapTemplate ldapTemplate() {
+	        return new LdapTemplate(contextSource());
+	    }
+ 
+	
+	
 	@Bean
 	public RepoAdditional repoAdditional() {
 		return new RepoImpl();
